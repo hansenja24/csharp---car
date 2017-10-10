1c73d9b611e1d5e6750146e402f6e28141282e2e
Hansen Hansen and Marcus Parmentier <marcus@>
2017-10-10 08:40:29 -0700

    Inital commit: Add constructor with default value for Miles.

---
+++ Car.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+
+class Car
+{
+  public string MakeModel;
+  public int Price;
+  public int Miles;
+
+  public Car(string carModel, int carPrice, int carMiles = 100)
+  {
+    MakeModel = carModel;
+    Price = carPrice;
+    Miles = carMiles;
+  }
+
+  public bool WorthBuying(int maxPrice)
+  {
+    return Price < (maxPrice + 100);
+  }
+}
+
+public class Program
+{
+  public static void Main()
+  {
+    Car porsche = new Car("2014 Porsche 911", 114991, 7864);
+    Car ford = new Car("2011 Ford F450", 55995, 14241);
+    Car lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+    Car mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+
+    List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
+
+    Console.WriteLine("Enter maximum price: ");
+    string stringMaxPrice = Console.ReadLine();
+    int maxPrice = int.Parse(stringMaxPrice);
+
+    Console.WriteLine("Enter maximum miles: ");
+    string stringMaxMiles = Console.ReadLine();
+    int maxMiles = int.Parse(stringMaxMiles);
+
+    List<Car> CarsMatchingSearch = new List<Car>(0);
+
+    foreach (Car automobile in Cars)
+    {
+      if (automobile.WorthBuying(maxPrice))
+      {
+        CarsMatchingSearch.Add(automobile);
+      }
+    }
+
+    foreach(Car automobile in CarsMatchingSearch)
+    {
+      Console.WriteLine(automobile.MakeModel);
+    }
+  }
+}
